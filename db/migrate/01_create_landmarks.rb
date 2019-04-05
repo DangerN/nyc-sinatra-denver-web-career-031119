32cf97a0133e92f85e6cf35210a5d0f9b236e24d
DangerN <cdneal091@gmail.com>
2019-04-05 14:08:32 -0600

    DB migrations

--- db/migrate/01_create_landmarks.rb
+++ db/migrate/01_create_landmarks.rb
@@ -1,3 +1,31 @@
 class CreateLandmarks < ActiveRecord::Migration
-  raise 'Write CreateLandmarks migration here'
+  # raise 'Write CreateLandmarks migration here'
+
+  def change
+
+    create_table :figures do |t|
+      t.string :name
+    end
+
+
+    create_table :titles do |t|
+      t.string :name
+    end
+
+    create_table :landmarks do |t|
+      t.string :name
+      t.references :figure
+      t.integer :year_completed
+    end
+
+
+
+    create_table :figure_titles do |t|
+      t.references :title
+      t.references :figure
+    end
+
+
+
+  end
 end
